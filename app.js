e26f3d1f378c39cf4f33b92f6ccf32f486095ac3
Nur Mohamed <mohamed.nur@student.greenriver.edu>
2025-02-11 15:01:38 -0800

    Finally Done

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-// Import the Express module
+// impoty Express
 import express from "express";
 
 // Instantiate an Express application
 const app = express();
 
-// Serve static files from the 'public' directoryh
+// Serve static files from the 'public' directory
 app.use(express.static("public"));
 
-//Define the port number where our server will listen
-const PORT = 3000;
+app.use(express.urlencoded({ extended: true }));
 
-//Create a "default" route for our webiste's home page
+// Define a port number for our server to listen on
+const PORT = "3000";
+
+// dEFINE A 'DEFAULT' route for our home page
 app.get("/", (req, res) => {
-  // send our home page as a response to teh client
+  // send our home page as a response to the client
   res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
-//Tell the server to listen on our specified port
+// Defina a 'Confirmation' route
+app.get("/confirmation", (req, res) => {
+  // send our thank you page
+  res.send("<h1>Thank You!</h1>");
+});
+
+// Tell server to listen on oiur specified port
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
